3dd79dbe086628be07b32351328ffb29407f08fc
Tejas Joshi <tejjoe98@gmail.com>
2024-05-22 22:13:46 -0700

    Update try_quant.py

--- try_quant.py
+++ try_quant.py
@@ -11,6 +11,7 @@ from pipeedge.quantization.basic_op import (
 from scipy.special import lambertw
 
 MONITORING_KEY_QUANT_ENCODE = 'quant_encode'
+MONITORING_KEY_QUANT_DECODE = 'quant_decode'
 
 def _quant_op(input_data, bit, mode='original'): ## TODO; create for new quant op (compute w_adptv)
     """
@@ -67,14 +68,15 @@ def get_exp_max(w_fp): ## TODO: need to compute per layer granularity normalized
 
     return exp_max, w_abs
 
+
 ### AdaptFloat implementation
-def get_quantized(bits, w_fp, e): # TODO: need to compute per layer granularity exp_bias
+def get_exp_bias(bits, w_fp, e): # TODO: need to compute per layer granularity exp_bias
     """returns exp_bias (the scaling factor for AdaptivFloat),
         val_max (matrix of new max value for datapt),
         val_min (matrix of new min value for datapt)
     params:
     bits : number of bits
-    exp :  number of exponenents
+    e :  number of exponents
     w_fp : full floating point weight matrix 
     """
     exp_max, w_abs = get_exp_max(w_fp)
@@ -86,7 +88,7 @@ def get_quantized(bits, w_fp, e): # TODO: need to compute per layer granularity 
     #     m_mat.append(m)
     # m_mat = torch.tensor(m_mat)
 
-    # number of mantissa bits
+    # number of mantissa bits TODO: create 8, 6 and 4 bit splits
     mants = bits - e - 1
 
     w_sign = torch.sign(w_fp)
@@ -114,6 +116,7 @@ def get_quantized(bits, w_fp, e): # TODO: need to compute per layer granularity 
 
     # rounding and clamping
     w_abs_clamped = torch.clamp(w_abs, min=val_min, max=val_max)
+    ##########
 
     # matrices of exponents
     w_exp = np.floor(np.log2(w_abs_clamped))
@@ -153,36 +156,6 @@ def _uint32_to_uint8(tensor):
     assert tensor.dtype == np.uint32
     return tensor.view('uint8')
 
-def tensor_encode(input_data: torch.Tensor, quant_bit: int) -> List[torch.Tensor]:
-    """
-        The input to the encoder should be a torch.Tensor
-        We first cast it to a np.array, then do everything else
-    """
-    quant_bit_tensor = torch.tensor(quant_bit, dtype = torch.int8)
-    if quant_bit == 0:
-        return [input_data, torch.tensor(input_data.shape), torch.tensor(1.0), torch.tensor(0.0),
-                quant_bit_tensor]
-
-    input_data = input_data.numpy()
-    shape = input_data.shape
-    # ensure the input is scaled to [0,1],
-    shift = input_data.min()
-    input_data = input_data - shift
-    scale_factor = input_data.max()
-    rescale_input = input_data/scale_factor
-    # quant
-    _, int_map = _quant_op(rescale_input, quant_bit)
-    comm_tensor = _intmap_encode(int_map, quant_bit)
-    # split uint32 into 4 uint8
-    comm_tensor = _uint32_to_uint8(comm_tensor)
-    # convert array to tensor for p2p communication
-    comm_tensor = torch.tensor(comm_tensor, dtype = torch.uint8)
-    shape = torch.tensor(shape, dtype = torch.int32)
-    scale_factor = torch.tensor(scale_factor, dtype = torch.float32)
-    shift = torch.tensor(shift, dtype = torch.float32)
-
-    # scale_factor is needed to restore the tensor
-    return [comm_tensor, shape, scale_factor, shift, quant_bit_tensor]
 
 def _clamp_factor_laplace(bit: int) -> torch.Tensor:
     # scipy returns a float64, but we'll overflow first if we don't force it when bit>=32
@@ -210,16 +183,13 @@ def clamp_banner2019_gelu(tensor: torch.Tensor, bit: int) -> torch.Tensor:
     alpha = _clamp_factor_gelu(bit).to(tensor) * dist_parameter
     return tensor.clamp(min=-alpha, max=alpha)
 
-def tensor_encode_outerdim(batched_tensor: torch.Tensor, quant_bit: int) -> List[torch.Tensor]:
-    """do quantization on each image in the micro-batched tensor with size [b,c,h,w]"""
-    list_of_lists = [tensor_encode(t, quant_bit) for t in batched_tensor]
-    encoded_tensors = list(zip(*list_of_lists))
-    return [torch.stack(t,0) for t in encoded_tensors]
+##########################################################################
+##########################################################################
 
 
-def forward_hook_quant_encode(module, _input_arg, output: Union[torch.Tensor, Tuple[torch.Tensor, ...]]):
-    """encode tensor in the forward hook (after each module)""" 
-
+# AdaptiveFloat integration: forward_hook_quant_encode
+def adptvflt_forward_hook_quant_encode(module, _input_arg, output: Union[torch.Tensor, Tuple[torch.Tensor, ...]]):
+    """encode tensor in the forward hook (after each module)"""
     monitoring.iteration_start(MONITORING_KEY_QUANT_ENCODE)
     if isinstance(output, torch.Tensor):
         output = (output,)
@@ -228,13 +198,188 @@ def forward_hook_quant_encode(module, _input_arg, output: Union[torch.Tensor, Tu
     comm_tuple = []
     for tensor in output:
         assert isinstance(tensor, torch.Tensor)
-        if quant_bit > 0: ## TODO: create new clamp operation using val_min, val_max and exp_bias for laplace dist
-            clamp = clamp_banner2019_laplace if tensor.min() < 0.2 else clamp_banner2019_gelu 
-            tensor = clamp(tensor, quant_bit)
-        stacked_tensor = tensor_encode_outerdim(tensor, quant_bit)
+        if quant_bit > 0:
+            clamp = adptvflt_clamp
+            tensor, weight_mat_signed, mantissa_bits = clamp(tensor, quant_bit)
+        stacked_tensor = adptvflt_tensor_encode_outerdim(tensor, quant_bit, weight_mat_signed, mantissa_bits)
         comm_tuple += stacked_tensor
     # Measure work as the microbatch size, but quantization only does work if quant_bit > 0.
     n_items = models.get_microbatch_size(output[0], verify=True) if quant_bit > 0 else 0
     monitoring.iteration(MONITORING_KEY_QUANT_ENCODE, work=n_items, accuracy=quant_bit)
     return tuple(comm_tuple)
 
+# AdaptiveFloat integration: forward_pre_hook_quant_decode
+def adptvflt_forward_pre_hook_quant_decode(_module, input_arg: Tuple[Tuple[torch.Tensor, ...]]):
+    """decode tensor in the preforward hook (before each module)"""
+    monitoring.iteration_start(MONITORING_KEY_QUANT_DECODE)
+    assert isinstance(input_arg, tuple)
+    assert len(input_arg) == 1
+    # input_tensor: len=5x for x tensors encoded as: comm_tensor, input_shape, scale_factor, shift, quant_bit; 
+    # for adptvflt integration: weight_map_mants_adptv, weight_map_exps_adptv, w_sign, input_shape, quant_bit
+    input_tensors = input_arg[0]
+    assert isinstance(input_tensors, tuple)
+    assert len(input_tensors)%5 == 0
+    assert len(input_tensors) >= 5
+    quant_bit = input_tensors[4][0].item() # assume the same quantization bitwidth for all items
+    forward_tensor = []
+    for i in range(len(input_tensors) // 5):
+        input_tensor = input_tensors[i*5:i*5+5]
+        batched_tensor = adptvflt_tensor_decode_outerdim(input_tensor)
+        forward_tensor.append(batched_tensor)
+    # Return value(s) should be wrapped in an outer tuple, like input_arg
+    # The tuple will be unpacked when forward() is invoked, which must yield a single parameter
+    if len(forward_tensor) == 1:
+        # assume that the original result was a single tensor rather than a tuple w/ len=1
+        outputs = tuple(forward_tensor)
+    else:
+        outputs = (tuple(forward_tensor),)
+    # Measure work as the microbatch size, but quantization only does work if quant_bit > 0.
+    n_items = models.get_microbatch_size(outputs, verify=True) if quant_bit > 0 else 0
+    monitoring.iteration(MONITORING_KEY_QUANT_DECODE, work=n_items, accuracy=quant_bit)
+    return outputs
+
+
+# AdaptFloat integration: get_exp_max
+def adptvflt_get_exp_max(w_fp): ## TODO: need to compute per layer granularity normalized exp_max 
+                   ## considering given constraint in paper
+    """returns normalized exp_max for max(w_abs)"""
+
+    # assert bit > 0
+    # assert np.all(input_data >= 0)
+    # assert np.all(input_data <= 1)
+
+    w_abs = w_fp.abs()
+
+    exp_max = math.log(w_abs.max() - 2)
+    assert exp_max > math.log(w_abs.max() - 2) - 1
+
+    return exp_max, w_abs
+
+# AdaptFloat integration: CLAMPING
+def adptvflt_clamp(tensor: torch.Tensor, bit: int) -> torch.Tensor:
+    """
+    params:
+    bits : number of bits
+    exp_bits :  number of exponent bits
+    tensor (w_fp) : full floating point weight matrix 
+    """
+    exp_max, w_abs = adptvflt_get_exp_max(tensor)
+
+    if bit == 8:
+        exp_bits = 4
+
+    if bit == 6:
+        exp_bits = 3
+
+    if bit == 4:
+        exp_bits = 2
+
+    # number of mantissa bits TODO: create 8, 6 and 4 bit splits
+    mant_bits = bit - exp_bits - 1
+
+    w_sign = tensor.sign()
+
+    # exp_bias computation
+    exp_bias = exp_max - (2**math.e - 1)
+
+    val_min = 2**exp_bias * (1 + (1/2**mant_bits))
+
+    val_max = 2**exp_max * (2 - (1/2**mant_bits))
+
+    # rounding and clamping
+    w_abs_clamped = tensor.clamp(min=val_min, max=val_max)
+
+    return w_abs_clamped, w_sign, mant_bits
+
+
+# AdaptFloat integration: quant_op
+def adptvflt_quant_op(input_data, mant_bits):
+
+    # assert np.all(input_data >= 0)
+    # assert np.all(input_data <= 1)
+
+    # matrices of exponents
+    input_log2 = input_data.log2()
+    w_exp = input_log2.floor()
+
+    # matrix of mantissas
+    w_mant = input_data / (2 ** w_exp)
+
+    # quantized and scaled
+    w_q = w_mant * (1/2**mant_bits)
+
+    return w_q, w_exp
+
+# AdaptFloat integration: tensor_encode 
+def adptvflt_tensor_encode(input_data: torch.Tensor, quant_bit: int, w_sign, mant_bits) -> List[torch.Tensor]:
+    """
+        The input to the encoder should be a torch.Tensor
+        We first cast it to a np.array, then do everything else
+    """
+    quant_bit_tensor = torch.tensor(quant_bit, dtype = torch.int8)
+    if quant_bit == 0:
+        return [input_data, torch.tensor(input_data.shape), torch.tensor(1.0), torch.tensor(0.0),
+                quant_bit_tensor]
+
+    # input_data = input_data.numpy()
+    shape = input_data.shape
+    # ensure the input is scaled to [0,1],
+    # shift = input_data.min()
+    # input_data = input_data - shift
+    # scale_factor = input_data.max()
+    # rescale_input = input_data/scale_factor
+
+    # quant
+    weight_map_mants_adptv, weight_map_exps_adptv = adptvflt_quant_op(input_data, mant_bits)
+    #comm_tensor = _intmap_encode(int_map, quant_bit)
+    # split uint32 into 4 uint8
+    #comm_tensor = _uint32_to_uint8(comm_tensor)
+    # convert array to tensor for p2p communication
+    #comm_tensor = torch.tensor(comm_tensor, dtype = torch.uint8)
+    shape = torch.tensor(shape, dtype = torch.int32) # TODO: maybe take out?
+    #scale_factor = torch.tensor(scale_factor, dtype = torch.float32)
+    #shift = torch.tensor(shift, dtype = torch.float32)
+
+    # scale_factor is needed to restore the tensor
+    return [weight_map_mants_adptv, weight_map_exps_adptv, w_sign]
+
+
+# AdaptFloat integration: adptvflt_tensor_decode 
+def adptvflt_tensor_decode(encodings: List[torch.Tensor]) -> torch.Tensor:
+    """
+        decode the compressed tensor with uint8 value
+    """
+    weight_map_mants_adptv, weight_map_exps_adptv, w_sign = encodings
+    # if quant_bit == 0:
+    #     return weight_map_adptv
+
+    # convert tensor to array for computation and splice uint8 to uint32
+    assert isinstance(weight_map_mants_adptv, torch.Tensor)
+    assert isinstance(weight_map_exps_adptv, torch.Tensor)
+    #comm_tensor = _uint8_to_uint32(comm_tensor.numpy())
+    # input_shape = input_shape.tolist()
+    #scale_factor = scale_factor.item()
+    #shift = shift.item()
+    # quant_bit = quant_bit.item()
+    #restore_int_map = _intmap_decode(comm_tensor, input_shape, quant_bit)
+    #restore_tensor = _intmap2float(restore_int_map, quant_bit)
+
+    # final output matrix
+    w_adptv = w_sign * 2**(weight_map_exps_adptv) * weight_map_mants_adptv
+
+    return w_adptv
+
+
+# AdaptFloat integration: tensor_encode_outerdim 
+def adptvflt_tensor_encode_outerdim(batched_tensor: torch.Tensor, quant_bit: int, w_sign, mant_bits) -> List[torch.Tensor]:
+    """do quantization on each image in the micro-batched tensor with size [b,c,h,w]"""
+    list_of_lists = [adptvflt_tensor_encode(t, quant_bit, w_sign, mant_bits) for t in batched_tensor]
+    encoded_tensors = list(zip(*list_of_lists))
+    return [torch.stack(t,0) for t in encoded_tensors]
+
+
+# AdaptFloat integration: tensor_decode_outerdim
+def adptvflt_tensor_decode_outerdim(batched_encodings: List[torch.Tensor]) -> torch.Tensor:
+    """decode the encoded tensor with multiple images in one batch, each encoded image data is in length of 5"""
+    tensors = [adptvflt_tensor_decode(encodings) for encodings in zip(*batched_encodings)]
+    return torch.stack(tensors, 0)
